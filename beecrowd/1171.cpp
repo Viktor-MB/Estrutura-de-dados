da9e8077de824cab869b028043c8fe0f966be9a9
Viktor-MB <viktor.mayerberruezo@gmail.com>
2025-08-27 13:32:57 -0300

    1171 não completo

--- beecrowd/1171.cpp
+++ beecrowd/1171.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include <cstdio>
+#include <vector>
 using namespace std;
 
 
+vector<int> numeros;
 
 int main(){
 
     int N;
     cin >> N; 
 
-    for(int i=0; i <= N; i++){
+    for(int i=0; i < N; i++){
 
-
-
+        int numero;
+        cin >> numero;
+        numeros.push_back(numero);
+        
+        
     }
+
+    for(int numero : numeros){
+        cout << numero << endl:
+    }
+
     return 0;
 }
