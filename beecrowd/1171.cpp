5295147003c03a5e1872ba74643803e15c1bf9d5
Viktor-MB <viktor.mayerberruezo@gmail.com>
2025-08-29 13:45:20 -0300

    Adcionando o exercicio 1171

--- beecrowd/1171.cpp
+++ beecrowd/1171.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
-#include <cstdio>
-#include <vector>
+#include <list>
+#include <map>
 using namespace std;
 
-
-vector<int> numeros;
-
-int main(){
-
+int main() {
     int N;
-    cin >> N; 
+    cin >> N;
 
-    for(int i=0; i < N; i++){
-
+    list<int> numeros;
+    for (int i = 0; i < N; i++) {
         int numero;
         cin >> numero;
         numeros.push_back(numero);
-        
-        
     }
 
-    for(int numero : numeros){
-        cout << numero << endl;
+    map<int, int> freq;
+    for (int numero : numeros) {
+        freq[numero]++;
+    }
+
+    for (auto it = freq.begin(); it != freq.end(); ++it) {
+        cout << it->first << " aparece " << it->second << " vez(es)" << endl;
     }
 
     return 0;
